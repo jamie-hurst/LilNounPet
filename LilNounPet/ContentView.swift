2d5e34963874e22692dddc9e42b7bb229507a8e9
warmrome <pxpx786@gmail.com>
2022-06-25 16:43:51 -0700

    Initial Commit

---
+++ LilNounPet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LilNounPet
+//
+//  Created by Jameson Hurst on 6/25/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LilNounPet/LilNounPetApp.swift
@@ -0,0 +1,17 @@
+//
+//  LilNounPetApp.swift
+//  LilNounPet
+//
+//  Created by Jameson Hurst on 6/25/22.
+//
+
+import SwiftUI
+
+@main
+struct LilNounPetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
