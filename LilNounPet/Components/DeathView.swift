9dca8b3975d2cfab67c710f95f5eedcc36a14a23
warmrome <pxpx786@gmail.com>
2022-07-05 19:54:23 -0700

    Tweaks to DeathView to make it look more refined.

--- LilNounPet/Components/DeathView.swift
+++ LilNounPet/Components/DeathView.swift
@@ -11,19 +11,17 @@ struct DeathView: View {
     @EnvironmentObject var vm : ViewModel
     
     var body: some View {
-        Section("Your pet has died. ☠️") {
-            Text("Here lies \(vm.pet.name), born \(vm.pet.birthday.formatted())")
-            Text("They lived to be \(vm.pet.ageAtDeath, specifier: "%.2f") days old.")
-    
-            Text("last meal: \(vm.pet.lastMeal.formatted())")
-            Text("last drink: \(vm.pet.lastDrink.formatted())")
+        
+        Section("\(vm.pet.name) is dead ☠️") {
+
+            Button("Respawn your pet") {
+                vm.toggleHatchView()
+                vm.disableNotifications()
+                vm.cancelNotifications()
+            }
         }
         
-        Button("Respawn your pet") {
-            vm.toggleHatchView()
-            vm.disableNotifications()
-            vm.cancelNotifications()
-        }
+        
     }
 }
 
