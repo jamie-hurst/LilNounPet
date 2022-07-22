ff3847d362811346def19886724c0fae3c47822d
warmrome <pxpx786@gmail.com>
2022-07-21 23:36:16 -0700

    refactoring & added default placeholder rather than it being blank. This should help with debugging.

---
+++ PetWidget/Provider.swift
@@ -0,0 +1,61 @@
+//
+//  Provider.swift
+//  PetWidgetExtension
+//
+//  Created by Jameson Hurst on 7/21/22.
+//
+
+import WidgetKit
+
+struct Provider: TimelineProvider {
+    
+    let defaultPet = Pet(name: "", bio: "", birthday: Date(), lastMeal: Date(), lastDrink: Date(), body: "body-cold", accessory: "accessory-1n", head: "head-aardvark", glasses: "glasses-hip-rose")
+    
+
+    func placeholder(in context: Context) -> SimpleEntry {
+        SimpleEntry(date: Date(), pet: defaultPet)
+    }
+    
+    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+        let entry = SimpleEntry(date: Date(), pet: readContents())
+        completion(entry)
+    }
+    
+    func getTimeline(in context: Context, completion: @escaping (Timeline<SimpleEntry>) -> Void) {
+        // Create a timeline entry for "now."
+        let date = Date()
+        let entry = SimpleEntry(
+            date: date,
+            pet: readContents()
+        )
+        
+        // Create a date that's 15 minutes in the future.
+        let nextUpdateDate = Calendar.current.date(byAdding: .minute, value: 15, to: date)!
+        
+        // Create the timeline with the entry and a reload policy with the date
+        // for the next update.
+        let timeline = Timeline(
+            entries:[entry],
+            policy: .after(nextUpdateDate)
+        )
+        
+        // Call the completion to pass the timeline to WidgetKit.
+        completion(timeline)
+    }
+    
+    func readContents() -> Pet {
+        let archiveURL = FileManager.sharedDocumentsDirectory.appendingPathComponent("SavedData")
+        
+        let decoder = JSONDecoder()
+        if let codeData = try? Data(contentsOf: archiveURL) {
+            do {
+                let contents = try decoder.decode(Pet.self, from: codeData)
+                return contents
+            } catch {
+                print("Error: Can't decode contents")
+            }
+        }
+        return Pet(name: "", bio: "", birthday: Date(), lastMeal: Date(), lastDrink: Date(), body: "", accessory: "", head: "", glasses: "")
+    }
+    
+}
