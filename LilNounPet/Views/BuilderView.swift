198f59d0ca7dffb2bb5418a0f0b73181b72010f9
warmrome <pxpx786@gmail.com>
2022-08-11 14:16:40 -0700

    Building out a simple view to allow for composing of a new pet using local state variables.

--- LilNounPet/Views/BuilderView.swift
+++ LilNounPet/Views/BuilderView.swift
@@ -7,32 +7,78 @@
 
 import SwiftUI
 
-
 struct BuilderView: View {
     @EnvironmentObject var vm : ViewModel
+    
+    @State private var selectedBody = 0
+    @State private var selectedAccessory = 0
+    @State private var selectedHead = 0
+    @State private var selectedGlasses = 0
+    
 
+    var ImageBuilder: some View {
+        ZStack {
+            
+            Group {
+                Image(vm.backgroundsArray[vm.chosenBackground])
+                    .interpolation(.none)
+                    .resizable()
+                    .scaledToFit()
+                
+                Image(bodiesArray[selectedBody])
+                    .interpolation(.none)
+                    .resizable()
+                    .scaledToFit()
+                
+                Image(accessoriesArray[selectedAccessory])
+                    .interpolation(.none)
+                    .resizable()
+                    .scaledToFit()
+                
+                Image(headsArray[selectedHead])
+                    .interpolation(.none)
+                    .resizable()
+                    .scaledToFit()
+                
+                Image(glassesArray[selectedGlasses])
+                    .interpolation(.none)
+                    .resizable()
+                    .scaledToFit()
+                
+            }
+            
+        }
+    }
+    
     
     var body: some View {
-        
-        Form {
-            ImageView()
-                .environmentObject(vm)
-            Text("Hi world")
+        VStack {
+            ImageBuilder
+            
+            Form {
+                Section("Body") {
+                    Stepper((bodiesArray[selectedBody]), value: $selectedBody, in: 0...(bodiesArray.count - 1))
+                    Slider(value: IntDoubleBinding($selectedBody).doubleValue, in: 0...Double(bodiesArray.count - 1), step: 1.0)
+                }
+                
+                Section("Accessory") {
+                    Stepper((accessoriesArray[selectedAccessory]), value: $selectedAccessory, in: 0...(accessoriesArray.count - 1))
+                    Slider(value: IntDoubleBinding($selectedAccessory).doubleValue, in: 0...Double(accessoriesArray.count - 1), step: 1.0)
+                }
+                
+                Section("Head") {
+                    Stepper((headsArray[selectedHead]), value: $selectedHead, in: 0...(headsArray.count - 1))
+                    Slider(value: IntDoubleBinding($selectedHead).doubleValue, in: 0...Double(headsArray.count - 1), step: 1.0)
+                }
+                
+                Section("Glasses") {
+                    Stepper((glassesArray[selectedGlasses]), value: $selectedGlasses, in: 0...(glassesArray.count - 1))
+                    Slider(value: IntDoubleBinding($selectedGlasses).doubleValue, in: 0...Double(glassesArray.count - 1), step: 1.0)
+                }
+                
+            }
         }
         
-//
-//        Section("Accessory") {
-//            Stepper((accessories[selectedAccessory]), value: $selectedAccessory, in: 0...(accessories.count - 1))
-//        }
-//
-//        Section("Head") {
-//            Stepper((heads[selectedHead]), value: $selectedHead, in: 0...(heads.count - 1))
-//            Slider(value: IntDoubleBinding($selectedHead).doubleValue, in: 0...Double(heads.count - 1), step: 1.0)
-//        }
-//
-//        Section("Glasses") {
-//            Stepper((glasses[selectedGlasses]), value: $selectedGlasses, in: 0...(glasses.count - 1))
-//        }
-       
+        
     }
 }
